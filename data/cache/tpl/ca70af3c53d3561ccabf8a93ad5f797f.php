7209289de969503a64024be4865ce9f4cb79c83b
wangwen1220 <wangwen1220@139.com>
2015-02-27 19:47:29 +0800

    'home'

--- data/cache/tpl/ca70af3c53d3561ccabf8a93ad5f797f.php
+++
@@ -1,3 +0,0 @@
-<?php exit;?>00145641282150922cfbe11a75aebb30da7a7fc8ab94s:261:"a:2:{s:8:"template";s:198:"<div class="g-ft">
-<?php $echoList = service("duxcms","Label","fragment",array( "app"=>"DuxCms", "label"=>"fragment", "mark"=>"foot"));  echo $echoList; ?>
-<p>Copyright © 2011-2014 DuxCms</p></div>";s:12:"compile_time";i:1424876821;}";
