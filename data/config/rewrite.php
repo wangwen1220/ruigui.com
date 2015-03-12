26c3ff9b209ab9e7958598743367771757f0c8db
wangwen1220 <wangwen1220@139.com>
2015-03-12 22:53:57 +0800

    'home'

--- data/config/rewrite.php
+++ data/config/rewrite.php
@@ -3,15 +3,15 @@ return array(
     /* URL规则 */
     'REWRITE_RULE' =>array(
         // 'list-<class_id>.html' => 'article/Category/index',
-        '<urlname>' => 'article/Category/index',
+        '<urlname>.html' => 'article/Category/index',
         // 'page-<class_id>.html' => 'page/Category/index',
-        '<urlname>.html' => 'page/Category/index',
+        '<urlname>' => 'page/Category/index',
         // 'article/<content_id>.html' => 'article/Content/index',
-        'product/<urltitle>.html' => 'article/Content/index',
+        'product/<urltitle> <content_id>.html' => 'article/Content/index',
         'form-<name>/<id>.html' => 'duxcms/Form/info',
         'form-<name>.html' => 'duxcms/Form/index',
         // 'tags-list/<name>.html' => 'duxcms/Tags/index',
-        '<name>.html' => 'duxcms/Tags/index',
+        '<name>' => 'duxcms/Tags/index',
         'tags/<name>.html' => 'duxcms/TagsContent/index',
     ),
 );
